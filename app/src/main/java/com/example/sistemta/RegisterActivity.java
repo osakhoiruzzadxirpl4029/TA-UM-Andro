a46a5522735649c5cd5cb84d1fd0978ae6ef15ec
Osa Khoiruzzad <khoiruzzad72@gmail.com>
2022-01-24 20:55:46 +0700

    Register Page

--- app/src/main/java/com/example/sistemta/RegisterActivity.java
+++ app/src/main/java/com/example/sistemta/RegisterActivity.java
@@ -2,7 +2,9 @@ package com.example.sistemta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+    public void onLoginClick(View view){
+        startActivity(new Intent(this, LoginActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
     }
 }
