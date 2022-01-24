cf8b9174f44e76d3e2c1e5ce15cb6260ec3dff3a
Osa Khoiruzzad <khoiruzzad72@gmail.com>
2022-01-24 17:08:19 +0700

    Register Button

--- app/src/main/java/com/example/sistemta/LoginActivity.java
+++ app/src/main/java/com/example/sistemta/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.sistemta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void onRegisterClick(View view){
+        startActivity(new Intent(this, RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
     }
 }
