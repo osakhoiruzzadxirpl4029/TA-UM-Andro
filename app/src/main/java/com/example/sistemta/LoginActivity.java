c0d03f86252bff1393a43e692c9f7dc09d022baf
osz <khoiruzzad72@gmail.com>
2022-02-10 21:13:40 +0700

    Register

--- app/src/main/java/com/example/sistemta/LoginActivity.java
+++ app/src/main/java/com/example/sistemta/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.sistemta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,15 +9,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    private EditText editTextEmail, editTextPassword;
-    private Button loginButton;
+    EditText mEmail, mPassword;
+    Button mLoginButton;
+    FirebaseAuth fAuth;
 
-    private FirebaseAuth mAuth;
-    private ProgressBar progressBar;
+
 
 
 
@@ -24,13 +29,39 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.editTextEmail);
+        mPassword = findViewById(R.id.editTextPassword);
+        mLoginButton = findViewById(R.id.loginButton);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPassword.getText().toString().trim();
+
+                fAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this,"Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
     public void onRegisterClick(View view){
         startActivity(new Intent(this, RegisterActivity.class));
         overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
     }
-    public void onMainClick(View view){
-        startActivity(new Intent(this, MainActivity.class));
-        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
-    }
+
 }
