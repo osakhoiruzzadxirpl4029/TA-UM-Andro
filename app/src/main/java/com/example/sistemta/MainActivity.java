fcde9e1046e54dcedc0cb4123f04ff9c20b9cf34
osz <khoiruzzad72@gmail.com>
2022-02-15 09:49:40 +0700

    Main

--- app/src/main/java/com/example/sistemta/MainActivity.java
+++ app/src/main/java/com/example/sistemta/MainActivity.java
@@ -7,64 +7,36 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Switch;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    Button mSol1, mSol2, mBuz1, mBuz2;
+    Switch sol, buz;
+    DatabaseReference mDatabase;
 
-    FirebaseDatabase root;
-    DatabaseReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sol = findViewById(R.id.sol1);
+        buz = findViewById(R.id.buz1);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        mSol1 = findViewById(R.id.sol1);
-        mBuz1 = findViewById(R.id.buzzer1);
-        mSol2 = findViewById(R.id.sol2);
-        mBuz2 = findViewById(R.id.buzzer2);
-
-        mSol1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                root = FirebaseDatabase.getInstance();
-                ref = root.getReference("Solenoid");
-                ref.setValue("1");
-            }
-        });
-
-        mSol2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                root = FirebaseDatabase.getInstance();
-                ref = root.getReference("Solenoid");
-                ref.setValue("0");
-            }
-        });
-
-        mBuz1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                root = FirebaseDatabase.getInstance();
-                ref = root.getReference("Buzzer");
-                ref.setValue("1");
-            }
-        });
-
-        mBuz2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                root = FirebaseDatabase.getInstance();
-                ref = root.getReference("Buzzer");
-                ref.setValue("0");
-            }
-        });
-
-
+        if (sol.isChecked()){
+            mDatabase.child("Solenoid").setValue("1");
+        }else{
+            mDatabase.child("Solenoid").setValue("0");
+        }
+        if (buz.isChecked()){
+            mDatabase.child("Buzzer").setValue("1");
+        }else{
+            mDatabase.child("Buzzer").setValue("0");
+        }
 
     }
+
 }
