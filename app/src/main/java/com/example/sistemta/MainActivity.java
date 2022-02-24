8a08c2cb46d1a8aa7c73f7cb52e58476044bd79c
Osa Khoiruzzad <khoiruzzad72@gmail.com>
2022-02-24 07:24:50 +0700

    Pop Up (not fixed) pt.2

--- app/src/main/java/com/example/sistemta/MainActivity.java
+++ app/src/main/java/com/example/sistemta/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.sistemta;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -16,7 +20,7 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
     Switch sol, buz;
     DatabaseReference mDatabase;
-
+    Dialog zDialog;
 
 
 
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        zDialog = new Dialog(this);
      //   mDatabase = FirebaseDatabase.getInstance().getReference();
 
       //  if (sol.isChecked()){
@@ -37,6 +42,19 @@ public class MainActivity extends AppCompatActivity {
          //   mDatabase.child("Buzzer").setValue("0");
         //}
 
+    }
+    public void ShowPopUp(View v){
+        TextView txtclose;
+        zDialog.setContentView(R.layout.popup_alarm);
+        txtclose = (TextView) zDialog.findViewById(R.id.closePop);
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                zDialog.dismiss();
+            }
+        });
+        zDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        zDialog.show();
     }
 
 }
