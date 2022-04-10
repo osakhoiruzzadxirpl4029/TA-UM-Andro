efa359eeb1b98365e55fe0ef0cc1c3f024e5b98b
Osa Khoiruzzad <khoiruzzad72@gmail.com>
2022-04-10 07:55:49 +0700

    Pop Up Window

--- app/src/main/java/com/example/sistemta/LoginActivity.java
+++ app/src/main/java/com/example/sistemta/LoginActivity.java
@@ -26,33 +26,33 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        // mEmail = findViewById(R.id.editTextEmail);
-        // mPassword = findViewById(R.id.editTextPassword);
-        // mLoginButton = findViewById(R.id.loginButton);
+        mEmail = findViewById(R.id.editTextEmail);
+        mPassword = findViewById(R.id.editTextPassword);
+        mLoginButton = findViewById(R.id.loginButton);
 
-        //fAuth = FirebaseAuth.getInstance();
+        fAuth = FirebaseAuth.getInstance();
 
-        //mLoginButton.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-         //   public void onClick(View view) {
-           //     String email = mEmail.getText().toString().trim();
-  //              String pass = mPassword.getText().toString().trim();
-//
-    //            fAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-      //              @Override
-        //            public void onComplete(@NonNull Task<AuthResult> task) {
-          //              if(task.isSuccessful()){
-            //                Toast.makeText(LoginActivity.this,"User Created", Toast.LENGTH_SHORT).show();
-              //              startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                //            overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
-                  //      }
-                    //    else{
-                      //      Toast.makeText(LoginActivity.this,"Error", Toast.LENGTH_SHORT).show();
-                        //}
-                    //}
-                //});
-            //}
-        //});
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPassword.getText().toString().trim();
+
+                fAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(LoginActivity.this,"User Created", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+                        }
+                        else{
+                            Toast.makeText(LoginActivity.this,"Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
     public void onRegisterClick(View view){
