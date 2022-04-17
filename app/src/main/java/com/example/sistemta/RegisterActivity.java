d8182ed02d6d689204d4a9aba1cf478525747039
Osa Khoiruzzad <khoiruzzad72@gmail.com>
2022-04-18 00:02:22 +0700

    Register Re-Fixing

--- app/src/main/java/com/example/sistemta/RegisterActivity.java
+++ app/src/main/java/com/example/sistemta/RegisterActivity.java
@@ -40,7 +40,7 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                String nama = mNama.getText().toString().trim();
+                String name = mNama.getText().toString().trim();
                 String email = mEmail.getText().toString().trim();
                 String pass = mPassword.getText().toString().trim();
 
@@ -48,12 +48,18 @@ public class RegisterActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
-                 //           User user = new User(nama,email);
-                 //           FirebaseDatabase.getInstance().getReference("User")
-                 //                   .child(FirebaseDatabase.getInstance().)
-                            Toast.makeText(RegisterActivity.this,"User Created", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                            overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+                            User user = new User(name,email);
+                            FirebaseDatabase.getInstance().getReference("User")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(RegisterActivity.this,"User Created", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                    overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+                                }
+                            });
+
                         }
                         else{
                             Toast.makeText(RegisterActivity.this,"Error Creating User", Toast.LENGTH_SHORT).show();
